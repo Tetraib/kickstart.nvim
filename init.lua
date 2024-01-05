cfc2bc3fa3b6bba65ad8ad98c98cca3d55f83ebd
Thibaut Constant <thibaut.constant@isisphinx.com>
2024-01-05 22:26:12 +0100

    add linux compile for fzf

--- init.lua
+++ init.lua
@@ -196,6 +196,15 @@ require('lazy').setup({
                 build =
                 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
             },
+            --        {
+            --            'nvim-telescope/telescope-fzf-native.nvim',
+            --            -- NOTE: If you are having trouble with this installation,
+            --            --       refer to the README for telescope-fzf-native for more instructions.
+            --            build = 'make',
+            --            cond = function()
+            --                return vim.fn.executable 'make' == 1
+            --            end,
+            --        },
         },
     },
 
